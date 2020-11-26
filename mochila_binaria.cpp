8a0b523e242f79ac1f78f4550e5e4cdd4eacbe4c
Guilherme <trintaeoitogc@gmail.com>
2020-11-26 16:18:27 -0300

    feat: criado a atribuição dos valores na main

--- mochila_binaria.cpp
+++ mochila_binaria.cpp
@@ -12,8 +12,13 @@ int[][] zerar_primeiras_colunas_e_linhas ( int m[][], int w, int n) {
 	return m;
 }
 
-
-
 int main() {
-	
+	int capacidade_mochila = 20;
+	int numero_itens = 5;
+	int beneficios[]	= {3, 5, 8, 4, 10};
+	int pesos = {2, 4, 5, 3, 9};
+
+	int max_valor = resolver_mochila_binaria();
+
+	std::cout << ": " << max_valor << std::endl;
 }
